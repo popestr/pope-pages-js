2fe24ec303207d7755ad5f445d84e74c97d0b39e
jballschneider <jballschneider@yext.com>
2022-08-12 18:32:01 +0000

    New path

--- src/templates/sports.tsx
+++ src/templates/sports.tsx
@@ -62,7 +62,7 @@ export const config: TemplateConfig = {
  * take on the form: featureName/entityId
  */
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  return `sport/${document.id.toString()}`;
+  return `sport/${document.id.toString()}/v2`;
 };
 
 /**
