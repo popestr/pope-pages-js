270b7f396b07a5a1de4fd146d78370f6ae0f379d
afriedman <afriedman@yext.com>
2022-12-20 21:06:21 +0000

    OnUrlChange plugin error

--- functions/onUrlChange/fieldUpdater.ts
+++ functions/onUrlChange/fieldUpdater.ts
@@ -12,6 +12,9 @@ export interface WritebackPayload {
 
 // TODO: Once JS config supports FunctionSelector, rename this and reference in feature
 export async function main(data: WritebackPayload) {
+
+  throw "Intentional OnUrlChange Error"
+
   if (!data.entityId || !data.locale) {
     return null;
   }
